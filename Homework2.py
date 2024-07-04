467c651a2fc2b564c97dac6769931ec1602aa382
IgorKopitsa <igor.kopitsa68@yandex.ru>
2024-07-04 16:02:25 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,7 @@
+name = 'Курс: Python,'
+total_task_count =12
+hours_count = 1.5
+one_task_count = 1.5 / 12
+print (name, 'всего задач:', total_task_count,',затрачено часов:', hours_count, ',среднее время выполнения:', one_task_count)
+
+
